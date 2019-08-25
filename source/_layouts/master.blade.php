111634cdde2a5cceed9157561ef5a29b8aa0c95b
WaveHack <email@wavehack.net>
2019-08-25 19:13:58 +0200

    Add meta description

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    {{--<meta name="description" content="WaveHack.net">--}}
+    <meta name="description" content="Software developer, open source enthusiast, Linux lover, Windows user.">
 
     <meta property="og:url" content="https://www.wavehack.net">
     <meta property="og:type" content="website">
