1fba4bc1b13f6988d48e51dc22910d099fbd0a5c
WaveHack <email@wavehack.net>
2021-06-20 21:37:46 +0200

    Remove todo

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -22,8 +22,6 @@
 
     @stack('meta')
 
-    {{-- todo: GA? --}}
-
     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,700,700i,800,800i" rel="stylesheet">
     <link rel="stylesheet" href="{{ mix('css/main.css') }}">
 </head>
